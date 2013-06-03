3574d17cc84b5481c0cb38bab3b55ec7d918fbcd
Vitaly Lavrov <lavrovvv@gmail.com>
2013-06-03 13:01:49 +0400

    AppRunner refactoring

--- src/main/scala/AppRunner.scala
+++ src/main/scala/AppRunner.scala
@@ -7,18 +7,13 @@ object AppRunner extends App {
   val filename = args.headOption getOrElse "input.txt"
   val inputFile = new File(filename)
 
+  def cellToString(index: CellIndex, cell: Cell) = s"${index.row}|${index.column} $cell"
+
   val result = for {
     input <- loadFile(inputFile)
-    resultSpreadsheet <- Resolver.resolve(input)
-  } yield
-    for((index, cell) <- resultSpreadsheet)
-      yield s"${index.row}|${index.column} $cell"
+    solvedSpreadsheet <- Resolver.resolve(input)
+  }
+    yield solvedSpreadsheet.map((cellToString _).tupled)
 
   result.fold(println("error"))(_.foreach(println))
-}
-
-object TestApp extends App {
-  println {
-    2 to 1
-  }
 }
