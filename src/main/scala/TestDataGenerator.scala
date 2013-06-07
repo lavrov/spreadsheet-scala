93f7f489891acaaf587863234bb62b0f5d21a229
Vitaly Lavrov <lavrovvv@gmail.com>
2013-06-07 12:35:36 +0400

    test data generator

--- src/main/scala/TestDataGenerator.scala
+++ src/main/scala/TestDataGenerator.scala
@@ -5,14 +5,22 @@ import java.util.concurrent.ForkJoinPool
 import scala.util.Random
 
 object TestDataGenerator extends App {
+  val rows = 10
+  val columns = 10
+
+  def randomOperation = Random.nextInt(300) match {
+    case 0 => (Random.nextInt(rows) + 1) + "|" + (Random.nextInt(columns) + 1)
+    case _ => Random.nextLong().abs.toString
+  }
+
   val fileWriter = new BufferedWriter(new FileWriter("input.txt"))
 
   for {
-    row <- 1 to 1000
-    column <-1 to 100
+    row <- 1 to rows
+    column <-1 to columns
   }
     fileWriter.write {
-      row + "|" + column + " " + List.fill(20)(Random.nextLong().abs).mkString(" + ") + "\n"
+      row + "|" + column + " " + List.fill(10)(randomOperation).mkString(" + ") + "\n"
     }
 
   fileWriter.close()
